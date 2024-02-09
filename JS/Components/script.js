e3150b14124cbb7d06f0f328e171fdf48a25e2ed
Cainã Vitor <caina.vitor@gmail.com>
2024-02-09 13:25:55 -0300

    commitando projeto

---
+++ JS/Components/script.js
@@ -0,0 +1,8 @@
+ const BtnMobile = document.getElementById('#btn-Mobile');
+
+function ToggleEvent() {
+    const nav = document.getElementById('#nav');
+    nav.classList.toggle('active');
+}
+
+BtnMoobile.addEventListener('click', Toggle);
